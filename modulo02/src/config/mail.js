7933db610ff0c2bd6a2166b4b22ba69b63f0c33d
tonnyrogers <antoniel15975@gmail.com>
2020-06-03 23:04:40 -0300

    Appointments / Mongo / Notifications & Nodemailer

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2485bf46e153e5',
+    pass: '994387c54d7647',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
