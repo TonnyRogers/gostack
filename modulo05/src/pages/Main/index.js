2e080a3e730b3f265a90035d6b3f35617b49a293
tonnyrogers <antoniel15975@gmail.com>
2020-06-07 22:16:41 -0300

    Finishing React Native App (List User Repos)

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,26 +1,155 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuários"
+export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    newUser: '',
+    users: [
+      {
+        name: 'Tony',
+        login: 'TonnyRogers',
+        bio: 'Back-end & Front-end developer',
+        avatar: 'https://avatars3.githubusercontent.com/u/37991230?v=4',
+      },
+      {
+        name: 'Diego',
+        login: 'diego3g',
+        bio: `CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+          desenvolvimento web e mobile.`,
+        avatar: 'https://avatars2.githubusercontent.com/u/2254731?v=4',
+      },
+      {
+        name: 'Tom Preston-Werner',
+        login: 'mojombo',
+        bio: null,
+        avatar: 'https://avatars0.githubusercontent.com/u/1?v=4',
+      },
+      {
+        name: 'Filipe Deschamps',
+        login: 'filipedeschamps',
+        bio: `Vou deixar você apaixonado por programação! `,
+        avatar: 'https://avatars0.githubusercontent.com/u/4248081?v=4',
+      },
+    ],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss();
+  };
+
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
+  render() {
+    const { users, newUser, loading } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuários"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton
+                loading={loading}
+                onPress={() => this.handleNavigate(item)}
+              >
+                <ProfileButtonText> Ver Perfil </ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
         />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
--- modulo05/src/pages/Main/styles.js
+++ modulo05/src/pages/Main/styles.js
@@ -4,6 +4,7 @@ import { RectButton } from 'react-native-gesture-handler';
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+  background: #fff;
 `;
 
 export const Title = styled.Text`
@@ -37,4 +38,58 @@ export const SubmitButton = styled(RectButton)`
   border-radius: 4px;
   padding: 0 12px;
 `;
-export const SubmitButtonText = styled.Text``;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  background: #7150c1;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
+`;
+
+export const ProfileButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 14px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
