19bfdd3a37211f4b1f783eaf6b947ddd1b083279
tonnyrogers <antoniel15975@gmail.com>
2020-06-12 19:58:40 -0300

    Back-end API Adjustments

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -66,13 +67,24 @@ class UserController {
     }
 
     try {
-      const { id, name, provider } = await user.update(req.body);
+      await user.update(req.body);
+
+      const { id, name, provider, avatar } = await User.findByPk(req.userId, {
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      });
 
       return res.json({
         id,
         name,
         email,
         provider,
+        avatar,
       });
     } catch (error) {
       return res.status(500).json({ error });
