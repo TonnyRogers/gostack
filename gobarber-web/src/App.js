b1311a911d0ac558da014ef94034864fc8073cc6
tonnyrogers <antoniel15975@gmail.com>
2020-06-13 14:00:48 -0300

    Config Reactotron / Redux / Saga

--- gobarber-web/src/App.js
+++ gobarber-web/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import history from '~/services/history';
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
