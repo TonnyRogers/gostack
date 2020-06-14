bafb68215fd73459e6e4f46648ee6b20dc5be50c
tonnyrogers <antoniel15975@gmail.com>
2020-06-14 11:19:50 -0300

    Form update profile handle

--- gobarber-web/src/pages/Profile/index.js
+++ gobarber-web/src/pages/Profile/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
   const profile = useSelector((state) => state.user.profile);
 
-  function handleData(data) {}
+  const dispatch = useDispatch();
 
-  const schema = Yup.object().shape({
-    name: Yup.string().required('Campo obrigatório'),
-    email: Yup.string().email('Email inválido').required('Campo obrigatório'),
-    password: Yup.string().min(6, 'A senha deve conter ao menos 6 caracteres'),
-  });
+  function handleData(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  // const schema = Yup.object().shape({
+  //   name: Yup.string().required('Campo obrigatório'),
+  //   email: Yup.string().email('Email inválido').required('Campo obrigatório'),
+  //   password: Yup.string().min(6, 'A senha deve conter ao menos 6 caracteres'),
+  // });
 
   return (
     <Container>
-      <Form initialData={profile} schema={schema} onSubmit={handleData}>
+      <Form initialData={profile} onSubmit={handleData}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <hr />
