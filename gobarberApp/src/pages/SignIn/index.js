8f9bf4531a370678068c52c3ae1358dd1808b69d
tonnyrogers <antoniel15975@gmail.com>
2020-06-14 22:45:04 -0300

    App Config / Routes / Inital Pages / ESLint

---
+++ gobarberApp/src/pages/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn = () => {
+  return <Text>Login</Text>;
+};
+
+export default SignIn;
