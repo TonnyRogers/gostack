07bc18994f4f29525787792a6c8d869e7b59b5af
tonnyrogers <antoniel15975@gmail.com>
2020-06-15 21:50:26 -0300

    StatusBar config

--- gobarberApp/src/index.js
+++ gobarberApp/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar backgroundColor="#7159c1" barStyle="light-content" />
+      <Routes />
+    </>
+  );
 }
