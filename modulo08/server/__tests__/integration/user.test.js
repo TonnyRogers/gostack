88ea1fe7f10655b6bb116ecad5ad0db371380713
tonnyrogers <antoniel15975@gmail.com>
2020-06-19 19:54:26 -0300

    Test: Handle with duplicated email user

--- modulo08/server/__tests__/integration/user.test.js
+++ modulo08/server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to resgiter a duplated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Antoniel',
+        email: 'antoniel15975@gmail.com',
+        password_hash: '112233',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Antoniel',
+        email: 'antoniel15975@gmail.com',
+        password_hash: '112233',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
