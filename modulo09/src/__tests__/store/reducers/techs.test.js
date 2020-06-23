afd2f65f293137f9bd9a90caec4ef1abf42b320a
tonnyrogers <antoniel15975@gmail.com>
2020-06-22 22:13:01 -0300

    Test: Reducers

---
+++ modulo09/src/__tests__/store/reducers/techs.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import reducer, { INITIAL_STATE } from '../../../store/modules/techs/reducer';
+import * as Techs from '../../../store/modules/techs/actions';
+
+describe('Techs reducer', () => {
+
+  it('ADD_TECH', () => {
+    const state = reducer(INITIAL_STATE,Techs.addTech('Node.js'));
+
+    expect(state).toStrictEqual(['Node.js']);
+  })
+});
