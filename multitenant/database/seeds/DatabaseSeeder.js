46c3e16b0bee7f440ada9a51f87f5b199f040caa
tonnyrogers <antoniel15975@gmail.com>
2020-06-27 23:40:59 -0300

    ACL Permission User Team

--- multitenant/database/seeds/DatabaseSeeder.js
+++ multitenant/database/seeds/DatabaseSeeder.js
@@ -15,13 +15,64 @@
 // const Factory = use('Factory')
 const User = use('App/Models/User')
 
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
+
 class DatabaseSeeder {
   async run () {
-    await User.create({
+    const user = await User.create({
       name: 'Antoniel',
       email: 'antoniel15975@gmail.com',
       password: '112233123'
     })
+
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar Membros'
+    })
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Criar Projetos'
+    })
+
+    const admin = await Role.create({
+      slug: 'admin',
+      name: 'Administrador'
+    })
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderador'
+    })
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitante'
+    })
+
+    await admin.permissions()
+      .attach([
+        createInvite.id,
+        createProject.id
+      ])
+
+    await moderator.permissions()
+      .attach([
+        createProject.id
+      ])
+
+    const team = await user.teams().create({
+      name: 'Dev Squad',
+      user_id: user.id
+    })
+
+    const teamJoin = await user.teamJoins()
+      .where('team_id', team.id)
+      .first()
+
+    await teamJoin.roles()
+      .attach([admin.id])
   }
 }
 
