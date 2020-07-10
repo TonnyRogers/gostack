da33998f8e52373289de553ba49594ae0395fbf8
tonnyrogers <antoniel15975@gmail.com>
2020-07-10 12:44:05 -0300

    Main page styling

--- multitenant-app/src/pages/Main/index.js
+++ multitenant-app/src/pages/Main/index.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container, Header, MenuButton, TeamButton, TeamTitle } from './styles';
 
 const Main = () => {
   const navigation = useNavigation();
+  const activeTeam = useSelector((state) => state.teams.active);
 
   navigation.setOptions({
     title: 'Home',
   });
 
   return (
-    <View style={{ backgroundColor: '#353940', height: '100%' }}>
-      <Text
-        style={{
-          color: '#FFF',
-          textAlign: 'center',
-          fontWeight: 'bold',
-          marginTop: 20,
-          fontSize: 20,
-        }}
-      >
-        Dashboard
-      </Text>
-    </View>
+    <Container>
+      <Header>
+        <MenuButton
+          onPress={() => {}}
+          hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }}
+        >
+          <Icon name="menu" size={24} color="#FFF" />
+        </MenuButton>
+        <TeamTitle>
+          {activeTeam ? activeTeam.name : 'Selecione o time'}
+        </TeamTitle>
+        <TeamButton
+          onPress={() => {}}
+          hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }}
+        >
+          <Icon name="group" size={24} color="#FFF" />
+        </TeamButton>
+      </Header>
+    </Container>
   );
 };
 
---
+++ multitenant-app/src/pages/Main/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #353940;
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  background: #1d2127;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px;
+`;
+
+export const MenuButton = styled.TouchableOpacity``;
+
+export const TeamTitle = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const TeamButton = styled.TouchableOpacity``;
