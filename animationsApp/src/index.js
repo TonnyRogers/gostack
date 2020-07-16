b8b68c295ad84f44d72e558c4dce2e0755a51270
tonnyrogers <antoniel15975@gmail.com>
2020-07-16 18:03:44 -0300

    React Native Animation (decay/spring)

--- animationsApp/src/index.js
+++ animationsApp/src/index.js
@@ -1,20 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {View, SafeAreaView, Animated, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [ballY, setBallY] = useState(new Animated.Value(0));
+  const [ball1Y, setBall1Y] = useState(new Animated.Value(0));
+  const [ball2Y, setBall2Y] = useState(new Animated.Value(0));
+  const [ball3Y, setBall3Y] = useState(new Animated.Value(0));
+
   useEffect(() => {
-    Animated.timing(ballY, {
-      toValue: 500,
+    // Timing (linear)
+    Animated.timing(ball1Y, {
+      toValue: 200,
       duration: 1000,
       useNativeDriver: false,
     }).start();
-  }, [ballY]);
+
+    // Spring (elastic)
+    Animated.spring(ball2Y, {
+      toValue: 200,
+      bounciness: 20,
+      useNativeDriver: false,
+    }).start();
+
+    // Decay
+    Animated.decay(ball3Y, {
+      velocity: 0.4,
+      useNativeDriver: false,
+    }).start();
+  }, [ball1Y, ball2Y, ball3Y]);
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Animated.View style={[styles.ball, {top: ballY}]} />
+        <Animated.View style={[styles.ball, {top: ball1Y}]} />
+        <Animated.View style={[styles.ball, {top: ball2Y}]} />
+        <Animated.View style={[styles.ball, {top: ball3Y}]} />
       </View>
     </SafeAreaView>
   );
@@ -31,12 +50,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 30,
+    flexDirection: 'row',
   },
   ball: {
     width: 70,
     height: 70,
     borderRadius: 35,
     backgroundColor: '#508',
+    margin: 10,
   },
 });
 
